bd3083501bf5d716e7f0a81897371203e0b91c0d
Edd12321 <sanduedi309@gmail.com>
2024-05-17 23:19:40 +0300

    Bump version number

--- src/config.hpp
+++ src/config.hpp
@@ -72,4 +72,4 @@ static std::string errmsg         = "syntax error: ";
 static std::string warnmsg        = "warning: ";
 static char here_prompt           = '>';
 static std::string default_prompt = "zrc% ";
-static std::string ver            = "zrc v0.9b";
+static std::string ver            = "zrc v1.0";
